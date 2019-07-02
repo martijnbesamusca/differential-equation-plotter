288db917ea122a1cff65c3fd9d7cc503f4c83f90
Martijn Besamusca <martijnbesamusca@gmail.com>
2019-07-02 10:56:04 +0200

    basic files added

---
+++ src/components/ModularPane.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <app-plot v-if="paneType === 'plot'"/>
+        <app-title-bar v-if="paneType === 'bar'"></app-title-bar>
+        <app-side-bar></app-side-bar>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue, Watch} from 'vue-property-decorator';
+    import AppPlot from 'AppPlot.vue';
+    import AppSideBar from 'AppSideBar.vue';
+    import AppTitleBar from 'AppTitleBar.vue';
+
+    @Component({
+        components: {
+            AppPlot,
+            AppSideBar,
+            AppTitleBar,
+        },
+    })
+    export default class ModularPane extends Vue {
+        public paneType = null;
+
+        public mounted() {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ModularPanes.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue, Watch} from 'vue-property-decorator';
+    @Component({
+        components: {
+        },
+    })
+    export default class ModularPanes extends Vue {
+        public mounted() {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
